4782d2dda018fc719958144b550f503b9d838f93
MitchScho <scho.mitch@gmail.com>
2022-05-05 04:57:05 +0000

    create query promise using values from command line

---
+++ bootcamp_app/students.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+const cohortName = process.argv[2];
+const maxResults = process.argv[3];
+
+pool
+  .query(
+    `
+    SELECT students.id as student_id, students.name as name, ${cohortName} as cohort
+    FROM students
+    JOIN cohorts ON cohorts.id = cohort_id
+    LIMIT ${maxResults};
+    `
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  });
